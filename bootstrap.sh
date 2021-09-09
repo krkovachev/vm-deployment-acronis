baba4fa1e47ebd2dd8a01cfc51c05777addb7bff
Kristiyan Kovachev <kovachevkristiyan@gmail.com>
2021-09-09 09:00:38 +0300

    Refactor code

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,2 @@
-# Create dest symbolic link
-ln -sfn '/vagrant' '/vagrant2'
-ln -sfn '/vagrant' '/vagrant3'
+# Copy files over /vagrant folder
 cp -a /vagrant2/. /vagrant/
-# Update Packages
-#yum -y update
-
-# Clean cached data
-#yum clean all
-
-#Install git
-#yum install -y git
